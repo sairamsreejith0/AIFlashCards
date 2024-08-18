0e669c545a84fd1618df4ea73255394fff291c41
sairamsreejith0 <sairamsreejith0@gmail.com>
2024-08-18 18:46:55 -0500

    UI changes

--- app/Dashboard/myCollection/page.js
+++ app/Dashboard/myCollection/page.js
@@ -122,7 +122,7 @@ const FlashcardCollection = () => {
       {selectedCollection ? (
         <div>
           <Box sx={{ display: "flex", marginLeft: 2, gap: 2 }}>
-            <Button onClick={handleBackClick} className="gradient-button">
+            <Button onClick={handleBackClick}  className="gradient-button">
               Back to Collections
             </Button>
             <Button
--- app/page.js
+++ app/page.js
@@ -2,7 +2,6 @@
 import { Button, Typography } from "@mui/material";
 import Page from './Dashboard/page';
 import '../app/globals.css';
-import { SignedIn, SignedOut, RedirectToSignIn } from "@clerk/nextjs";
 
 export default function Home() {
   return (
